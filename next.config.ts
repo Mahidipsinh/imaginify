bf82b0cf5f425ffb2d685c7744c85d5c15af2daf
mahidipsinh <zalamahidipsinh007@gmail.com>
2025-06-10 15:42:03 +0530

    refactor: remove outdated configuration and comments from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,4 @@
 import type { NextConfig } from "next";
-// next.config.js
-
-module.exports = {
-  reactStrictMode: true,
-  eslint: {
-      ignoreDuringBuilds: true,
-  },
-}
-
 
 const nextConfig: NextConfig = {
   /* config options here */
